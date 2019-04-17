8b56c07667c8ef9d3bcdf3d94a1b28757da73c1a
yoginath4u <yogesh.bhaikatti@gmail.com>
2019-04-17 15:37:25 +0530

    my first program

---
+++ if.py
@@ -0,0 +1,18 @@
+
+age=30
+if age>=30:
+    print("Your age is greater than 29")
+    print('OK')
+age=40
+##pass statement is used to do nothing
+if age<=30:
+    pass25
+else:
+    print("your age is greater than 30")
+
+
+name=input("enter your name")
+if name:
+    print(f"your name is {name}")
+else:
+    print("Please enter your name")
