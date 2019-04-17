40c801bb9ffeca9d5f769631e308c2eb23f8f6a3
yoginath4u <yogesh.bhaikatti@gmail.com>
2019-04-17 16:19:01 +0530

    looping program

---
+++ loop.py
@@ -0,0 +1,28 @@
+
+num=1
+while num<=10:
+    print(num)
+    num+=1
+
+
+num=1
+while num<=10:
+    if num==5:
+        print(num)
+    num+=1
+
+
+num=1
+while num<=10:
+    if num<=5:
+        num+=1
+        continue
+    print(num)
+    num+=1
+
+for i in range(1,10,2):
+    print(i)
+
+name="yoginath"
+for i in name:
+    print(i)
