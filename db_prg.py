fb3b25e4e4d4213f0eb6cedac3595c7bfec0387c
yoginath4u <49714879+yoginath4u@users.noreply.github.com>
2019-04-30 20:08:21 +0530

    Delete db_prg.py

--- db_prg.py
+++
@@ -1,8 +0,0 @@
-import cx_Oracle
-conn=cx_Oracle.connect("yogi/yogi@localhost/orcl")
-cur=conn.cursor()
-cur.execute("insert into yogi values(3,'c',200)")
-cur.execute("commit")
-cur.execute("select * from yogi")
-for data in cur:
-    print(data)
